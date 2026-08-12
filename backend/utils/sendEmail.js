d801437017a806fcd42de4b205e6eaa578f5c9e9
Rohit Pareek <itrohitpareek@gmail.com>
2026-08-12 18:22:17 +0530

    fix gmail smtp connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -9,15 +9,19 @@ const getTransporter = () => {
   if (!process.env.EMAIL_USER || !process.env.EMAIL_APP_PASSWORD) {
     return null;
   }
+
   if (!transporter) {
     transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
       },
     });
   }
+
   return transporter;
 };
 
